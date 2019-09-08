451b01b372a4a96e8e3e645100234ea201cc6f6e
Matthew <mfarmer5102@gmail.com>
2019-09-08 16:06:28 -0400

    Collapsed header component into Main.js to allow for easier state changing when link clicked; verified clicking the About link toggles its visibility

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Main from "./components/Main.js";
-import Header from "./components/Header";
 
 class App extends Component {
   constructor(props) {
@@ -10,10 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="bg-light">
-        <Header />
-        <div className="container animated fadeInUpBig">
-          <Main />
-        </div>
+        <Main />
       </div>
     );
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -18,9 +18,10 @@ class Main extends Component {
     this.state = {
       totalClicks: 0,
       subComponentVisibilityToggler: {
-        AboutMe: true,
         Applications: true,
-        Connect: true
+        Skills: false,
+        Connect: false,
+        AboutMe: false
       }
     };
     this.tallyCumulativeClicks = this.tallyCumulativeClicks.bind(this);
@@ -61,27 +62,70 @@ class Main extends Component {
 
   render() {
     return (
-      <div className="p-3">
-        <div onClick={this.toggleVisibilityForAll} class="text-right mb-3">
-          <i class="fas fa-eye-slash fa-2x"></i>
-        </div>
-        <div class="alert alert-secondary" role="alert">
-          Total Clicks Across All Components: {this.state.totalClicks}
-        </div>
-        <Fetcher></Fetcher>
-        <AboutMe
-          visibility={this.state.subComponentVisibilityToggler.AboutMe}
-          cumulativeClicker={this.tallyCumulativeClicks}
-        ></AboutMe>
-        <Applications
-          visibility={this.state.subComponentVisibilityToggler.Applications}
-          cumulativeClicker={this.tallyCumulativeClicks}
-        ></Applications>
-        <Connect
-          visibility={this.state.subComponentVisibilityToggler.Connect}
-          cumulativeClicker={this.tallyCumulativeClicks}
-        ></Connect>
-      </div>
+      <main>
+        {/* Header ################################################################## */}
+        <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
+          <a class="navbar-brand" href="#">
+            <i className="fab fa-react"></i>
+            <span> Portfolio</span>
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">
+                  Applications
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Skills
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Connect
+                </a>
+              </li>
+              <li class="nav-item" onClick={this.toggleVisibilityForAll}>
+                <a class="nav-link" href="#">
+                  About
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        {/* Content Section ################################################################## */}
+        <section className="container animated fadeInUpBig p-3">
+          <div onClick={this.toggleVisibilityForAll} class="text-right mb-3">
+            <i class="fas fa-eye-slash fa-2x"></i>
+          </div>
+          <div class="alert alert-secondary" role="alert">
+            Total Clicks Across All Components: {this.state.totalClicks}
+          </div>
+          <Fetcher></Fetcher>
+          <AboutMe
+            visibility={this.state.subComponentVisibilityToggler.AboutMe}
+            cumulativeClicker={this.tallyCumulativeClicks}
+          ></AboutMe>
+          <Applications
+            visibility={this.state.subComponentVisibilityToggler.Applications}
+            cumulativeClicker={this.tallyCumulativeClicks}
+          ></Applications>
+          <Connect
+            visibility={this.state.subComponentVisibilityToggler.Connect}
+            cumulativeClicker={this.tallyCumulativeClicks}
+          ></Connect>
+        </section>
+      </main>
     );
   }
 }
